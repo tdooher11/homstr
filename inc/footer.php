ba15074ba663653a4d36c6b7680d579054f17c28
tim.dooher <tim.dooher@jivesoftware.com>
2015-11-14 12:21:17 -0800

    added blog link to footer and removed footer ad

--- inc/footer.php
+++ inc/footer.php
@@ -6,16 +6,16 @@
 		<aside style="position: absolute; left: 0px; top: 48px;" id="text-3" class="widget widget_text masonry-brick">			<div class="textwidget"><div style="width:700px;margin-left:auto;margin-right:auto;">
 
 
-                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                     <!-- footer ad -->
-                    <ins class="adsbygoogle"
+                   <!-- <ins class="adsbygoogle"
                          style="display:block"
                          data-ad-client="ca-pub-9485592799923193"
                          data-ad-slot="4023179467"
                          data-ad-format="auto"></ins>
                     <script>
                         (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
+                    </script> -->
 
                     </div></div>
 		</aside>	</div><!-- #footer-sidebar -->
@@ -25,11 +25,12 @@
 				
 			<div id="content-sidebar" class="content-sidebar widget-area" role="complementary">
 	<aside id="nav_menu-2" class="widget widget_nav_menu"><div class="menu-footer-container"><ul id="menu-footer" class="menu"><li id="menu-item-19" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-19"><a href="index.php">Home</a></li>
-<li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a href="aboutus.php">About Us</a></li>
-<li id="menu-item-49" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-49"><a href="news/">News</a></li>
-<li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a href="faq.php">FAQ</a></li>
-<li id="menu-item-35" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-35"><a href="contactus.php">Contact Us</a></li>
-<li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-21"><a href="privacy_policy.php">Privacy Policy</a></li>
+
+				<li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a href="http://blog.homstr.com">Blog</a></li>
+				<li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a href="aboutus.php">About Us</a></li>
+				<li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a href="faq.php">FAQ</a></li>
+				<li id="menu-item-35" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-35"><a href="contactus.php">Contact Us</a></li>
+				<li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-21"><a href="privacy_policy.php">Privacy Policy</a></li>
 </ul></div></aside></div><!-- #content-sidebar -->
 			</div><!-- .site-info -->
 		</footer>
