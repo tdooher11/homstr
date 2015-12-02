860a14d6d1f9e93b5935fd6e3f0563cb804b2947
tim.dooher <tim.dooher@jivesoftware.com>
2015-12-02 14:15:38 -0800

    reverting last couple commits

--- inc/footer.php
+++ inc/footer.php
@@ -1,32 +1,31 @@
 <footer id="colophon" class="site-footer" role="contentinfo">
-	<div class="site-info">
-		<div id="content-sidebar" class="content-sidebar widget-area" role="complementary">
-			<aside id="nav_menu-2" class="widget widget_nav_menu">
-				<div class="menu-footer-container">
-					<ul id="menu-footer" class="menu">
-						<li id="menu-item-19" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-19"><a href="index.php">Home</a></li>
-						<li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a href="http://blog.homstr.com">Blog</a></li>
-						<li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a href="aboutus.php">About Us</a></li>
-						<li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a href="faq.php">FAQ</a></li>
-						<li id="menu-item-35" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-35"><a href="contactus.php">Contact Us</a></li>
-						<li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-21"><a href="privacy_policy.php">Privacy Policy</a></li>
-					</ul>
-				</div>
-			</aside>
-		</div>
-		<!-- #content-sidebar -->
-	</div>
-</footer>
+
+			
 
 
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.3/angular.min.js"></script>
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/jquery.singlePageNav.min.js"></script>
-<script type="text/javascript" src="js/jquery.superslides.min.js"></script>
-<script type="text/javascript" src="js/jquery.countdown.min.js"></script>
-<script type="text/javascript" src="js/functions.js"></script>
-<script type="text/javascript" src="js/wow.min.js"></script>
-<script type="text/javascript" src="js/custom.js?v=1"></script>
-<script type="text/javascript" src="js/app.js"></script>
+			<div class="site-info">
+				
+			<div id="content-sidebar" class="content-sidebar widget-area" role="complementary">
+	<aside id="nav_menu-2" class="widget widget_nav_menu"><div class="menu-footer-container"><ul id="menu-footer" class="menu"><li id="menu-item-19" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-19"><a href="index.php">Home</a></li>
+
+				<li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a href="http://blog.homstr.com">Blog</a></li>
+				<li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a href="aboutus.php">About Us</a></li>
+				<li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a href="faq.php">FAQ</a></li>
+				<li id="menu-item-35" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-35"><a href="contactus.php">Contact Us</a></li>
+				<li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-21"><a href="privacy_policy.php">Privacy Policy</a></li>
+</ul></div></aside></div><!-- #content-sidebar -->
+			</div><!-- .site-info -->
+		</footer>
+		
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
+			<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.3/angular.min.js"></script>
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/jquery.singlePageNav.min.js"></script>
+		<script type="text/javascript" src="js/jquery.superslides.min.js"></script>
+		<script type="text/javascript" src="js/jquery.countdown.min.js"></script>
+		<script type="text/javascript" src="js/functions.js"></script>
+		<script type="text/javascript" src="js/wow.min.js"></script>
+		<script type="text/javascript" src="js/custom.js?v=1"></script>
+		<script type="text/javascript" src="js/app.js"></script>
 		
