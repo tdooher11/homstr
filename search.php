938326986ad15586c67e3c10312816e072fcb093
tim.dooher <tim.dooher@jivesoftware.com>
2015-12-04 09:37:30 -0800

    split navbar to separate file

--- search.php
+++ search.php
@@ -155,44 +155,9 @@ ob_start (); // Buffer output
 
 </script>
 <body class="home blog list-view full-width footer-widgets grid site" onload="initialize1()">
-	<header id="masthead" class="site-header" role="banner">
-		<div class="header-main">
-			<h1 class="site-title"><a href="index.php" rel="home"><img src="img/logo.png" alt="Homstr"></a></h1>
-
-			<div class="search-toggle">
-				<a href="#search-container" class="screen-reader-text" aria-expanded="false" aria-controls="search-container">Search</a>
-			</div>
-
-			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-				<button class="menu-toggle">Primary Menu</button>
-				<a class="screen-reader-text skip-link" href="#content">Skip to content</a>
-				<div class="menu-top-container">
-					<ul id="primary-menu" class="nav-menu">
-						<li id="menu-item-8" class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-home menu-item-8"><a href="index.php">Home</a></li>
-                        <li id="menu-item-42" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42"><a href="http://blog.homstr.com">Blog</a></li>
-                        <li id="menu-item-7" class="menu-item-7"><a href="aboutus.php">About Us</a></li>
-						<li id="menu-item-37" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-37"><a href="faq.php">FAQ</a></li>
-						<li id="menu-item-42" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42"><a href="news/">News</a></li>
-						<li id="menu-item-32" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-32"><a href="contactus.php">Contact Us</a></li>
-					</ul>
-				</div>			
-			</nav>
-		</div>
-
-		<div id="search-container" class="search-box-wrapper hide">
-			<div class="search-box">
-				<form name="search-form3" id="searchform3" action="search.php" method="get">
-							<div class="input-group">
-								<input autocomplete="off" name="search" id="autocomplete3" placeholder="Search Apartments and Homes..." class="form-control input-lg" style="border: 2px solid rgb(88, 113, 218);" type="text">
-								<span class="input-group-btn">
-									<a class="btn btn-store" onclick="$('form').submit();" style="font-size: 19px; background: rgb(0, 0, 0) none repeat scroll 0% 0%; color: rgb(255, 255, 255);">Go</a>
-								</span>
-							</div>
-						
-				</form>
-			</div>
-		</div>
-	</header>
+    <?php
+        include("inc/navbar.php");
+    ?>
 
 <div id="main" class="site-main">
 
